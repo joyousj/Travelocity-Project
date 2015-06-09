ee5d1918552628380b468e1f36ec85b1822d821d
joyousj <jsingharath82@gmail.com>
2015-06-09 13:48:20 -0500

    Create EventSpaceDAO.java

---
+++ EventSpaceDAO.java
@@ -0,0 +1,23 @@
+package com.travelocity.dao;
+
+import java.util.List;
+
+import com.travelocity.model.EventSpace;
+
+public interface EventSpaceDAO {
+	
+	public EventSpace getEventSpace(String name);
+	
+	public EventSpace getEventSpaceById(int id);
+	
+	public List<EventSpace> getEventSpaceByPrice(int range);
+	
+	public List<EventSpace> getEventSpaceByLocation(String location);
+	
+	public EventSpace getDescription(String description);
+	
+	public List<EventSpace> getEventSpaceByPriceAndLocation(int range, String location);
+	
+	public List<EventSpace> getEventSpaces();
+
+}
