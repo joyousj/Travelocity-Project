c2c8bf6575db51f3349d68d438f93725c224b471
joyousj <jsingharath82@gmail.com>
2015-06-09 13:52:57 -0500

    Create User.java

---
+++ User.java
@@ -0,0 +1,92 @@
+package com.travelocity.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="USER")
+public class User {
+	
+	@Id
+    @GeneratedValue
+    @Column(name="ID")
+    private Integer id;
+	
+	@Column(name="FIRST_NAME")
+	private String first_name;
+	
+	@Column(name="LAST_NAME")
+	private String last_name;
+	
+	@Column(name="COMPANY")
+	private String company;
+	
+	@Column(name="PHONE")
+	private long phone;
+	
+	@Column(name="EMAIL")
+	private String email;
+	
+	@Column(name="PASSWORD")
+	private String password;
+	
+	@Column(name="JOINED")
+	private Date joined;
+
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getFirst_name() {
+		return first_name;
+	}
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+	public String getLast_name() {
+		return last_name;
+	}
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	public long getPhone() {
+		return phone;
+	}
+	public void setPhone(long phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Date getJoined() {
+		return joined;
+	}
+	public void setJoined(Date joined) {
+		this.joined = joined;
+	}
+	
+}
